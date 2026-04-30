30c03df70d3e000f31c12f5ece2537650854cbd5
igdev <dorultanianos@gmail.com>
2026-04-30 17:04:55 +0300

    add inter

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import "@/styles/global.css"
 
+import { Inter } from 'next/font/google'
+
 export const metadata = {
   description: 'A blank template using Payload in a Next.js app.',
   title: 'Payload Blank Template',
 }
-
+const inter = Inter({weight: ["600", "400"]})
 export default async function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props
   return (
     <html lang="en">
-      <body>
+      <body className={`${inter.className}`}>
         <main>{children}</main>
       </body>
     </html>
