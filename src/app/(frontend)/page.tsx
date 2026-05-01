e46802cf7361cce91e67699f2908ed467348df5f
igdev <dorultanianos@gmail.com>
2026-05-01 10:20:51 +0300

    wip exploration

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -9,13 +9,14 @@ export default async function HomePage() {
   const headers = await getHeaders()
   const payloadConfig = await config
   const payload = await getPayload({ config: payloadConfig })
-  const { user } = await payload.auth({ headers });
+  const { user } = await payload.auth({ headers })
   const profile = await payload.findGlobal({ slug: 'profile' })
-  const html = convertLexicalToHTML({ data: profile.bio })
+  const __html = convertLexicalToHTML({ data: profile.bio })
+
   return (
     <div className="p-2">
       <h1>Bio:</h1>
-      <div dangerouslySetInnerHTML={{ __html: html }} />
+      <div dangerouslySetInnerHTML={{ __html }} />
       <h1 className="mt-4">Skills:</h1>
       {profile.skills?.map((skill) => (
         <div key={skill.id}>{skill.name}</div>
